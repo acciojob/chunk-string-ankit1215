887c48b85f30a364af12702e8e5774e3398deb01
Ankit Pandit <58819767+ankit1215@users.noreply.github.com>
2025-12-05 17:38:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function stringChop(str, size) {
-  // your code here
+  if (!str) return []; 
+  size = Number(size); // Ensure the chunk size is a number
+  const result = [];
+
+  for (let i = 0; i < str.length; i += size) {
+    result.push(str.slice(i, i + size));
+  }
+
+  return result;
 }
 
 // Do not change the code below
